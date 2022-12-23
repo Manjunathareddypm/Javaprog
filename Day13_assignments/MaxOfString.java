c2a57941b6d2d740ffbd72105e28111b2aa09588
Manjunatha Reddy PM <mreddypm@gmail.com>
2022-12-23 12:09:47 +0530

    added

---
+++ Day13_assignments/MaxOfString.java
@@ -0,0 +1,18 @@
+package com.Day13;
+
+public class MaxOfString {
+    public static String maximum(String str1,String str2, String str3){
+        String max = str1;
+        if(str2.compareTo(max)>0){
+            max = str2;
+        }
+        if(str3.compareTo(max)>0){
+            max = str3;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        String str1 = "Apple", str2 = "Peach", str3 = "Banana";
+        System.out.println(maximum(str1,str2,str3));
+    }
+}
